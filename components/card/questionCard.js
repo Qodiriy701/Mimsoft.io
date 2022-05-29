eb3e817ee40302b6af58a17c4a39132a53e8aeea
Qosim Qodirov <qosimqodiriy701@gmail.com>
2022-05-30 02:22:06 +0500

    test 3

--- components/card/questionCard.js
+++ components/card/questionCard.js
@@ -6,10 +6,12 @@ function QuestionCard({ question, index }) {
 
     return (
         <div className="accordion-item bg-bgblack text-white Black border-none">
-            <div  id={`flush-heading${index}`} className="accordion-header text-white divBox">
-                <p className="accordion-button collapsed text-white text-xl font-medium pBox" type="button" data-bs-toggle="collapse" data-bs-target={`#flush-collapse${index}`} aria-expanded="false" aria-controls={`flush-collapse${index}`}>
-                    {question.title[i18n.language]}
-                </p>
+            <div>
+                <div id={`flush-heading${index}`} className="accordion-header text-white divBox">
+                    <p className="accordion-button collapsed text-white text-xl font-medium pBox" type="button" data-bs-toggle="collapse" data-bs-target={`#flush-collapse${index}`} aria-expanded="false" aria-controls={`flush-collapse${index}`}>
+                        {question.title[i18n.language]}
+                    </p>
+                </div>
             </div>
             <div id={`flush-collapse${index}`} className="accordion-collapse collapse bg-bgblack border-none" aria-labelledby={`flush-heading${index}`} data-bs-parent="#accordionFlushExample">
                 <p className="accordion-body text-placeholderColor text-base" dangerouslySetInnerHTML={{ __html: question.body[i18n.language] }}></p>
